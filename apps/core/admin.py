691a2b4dd6bfc3a53355b14cb1ff8a80f15355fc
zahidulhasan123 <zahidul_hasan@cse.green.edu.bd>
2026-09-08 12:06:21 +0600

    changed title of admin panel

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+admin.site.site_header = "Oxford Vocab BD Admin"
+admin.site.site_title = "Oxford Vocab BD Admin"
+admin.site.index_title = "Oxford Vocab BD Admin"
+
 # Register your models here.
